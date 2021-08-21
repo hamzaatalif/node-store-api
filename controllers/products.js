5f8ce8283f9cc501c05d55578f4e5633bd656c46
hamzaatalif <hamzaatalif@gmail.com>
2021-08-21 16:31:36 +0500

    regex setup

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,25 @@
-const getAllProductsStatic = async (req,res) => {
-    res.status(200).json({msg: "All products are for testing here."})
+const Product = require("../models/product");
+
+
+const getAllProductsStatic = async (req,res) => {    
+    const products = await Product.find({}).sort("name") 
+    res.status(200).json({products})
 }
 
 const getAllProducts = async (req,res) => {
-    res.status(200).json({msg: "All products are real here."})
+    const {featured, company, name} = req.query;
+    const queryObject = {}
+    if (featured){
+        queryObject.featured = featured === 'true' ? true : false;
+    }    
+    if (company){
+        queryObject.company = company
+    }
+    if (name){
+        queryObject.name = { "$regex": "/name/", "$options": "i"}
+    } 
+    const products = await Product.find(req.query);
+    res.status(200).json({products})
 }
 
 
