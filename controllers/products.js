bd1a6867af4bd083584bb38c545d044608d545e4
hamzaatalif <hamzaatalif@gmail.com>
2021-08-24 00:39:42 +0500

    sort limit skip added

--- controllers/products.js
+++ controllers/products.js
@@ -2,12 +2,12 @@ const Product = require("../models/product");
 
 
 const getAllProductsStatic = async (req,res) => {    
-    const products = await Product.find({}).sort("name") 
+    const products = await Product.find({}).select("name") 
     res.status(200).json({products})
 }
 
 const getAllProducts = async (req,res) => {
-    const {featured, company, name} = req.query;
+    const {featured, company, name, sort, fields} = req.query;
     const queryObject = {}
     if (featured){
         queryObject.featured = featured === 'true' ? true : false;
@@ -16,10 +16,28 @@ const getAllProducts = async (req,res) => {
         queryObject.company = company
     }
     if (name){
-        queryObject.name = { "$regex": "/name/", "$options": "i"}
+        queryObject.name = { $regex: name, $options: 'i'}
     } 
-    const products = await Product.find(req.query);
-    res.status(200).json({products})
+    let result = Product.find(queryObject);
+    if (sort){
+        const sortList = sort.split(",").join(" ");
+        result = result.sort(sortList)
+    }
+    else {
+        result = result.sort("createdAt")
+    }
+
+    if(fields){
+        const fieldsList = fields.split(",").join(" ");
+        result = result.select(fieldsList)
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 15;
+    const skip = (page - 1) * limit ;
+    result = result.skip(skip).limit(limit) ;
+    const products = await result
+    res.status(200).json({products: products,nmOfHits: products.length})
 }
 
 
